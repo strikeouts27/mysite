f7e232cd35cca10289d647446a9f4a415924d498
strikeouts27 <stribling.andrew@gmail.com>
2023-07-11 15:08:13 -0500

    part 5 of tutorial complete, unittest and django

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,9 +1,12 @@
 from pathlib import Path
-import os
+import os 
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+SECRET_KEY = os.environ['SECRET_KEY']
+# https://stackoverflow.com/questions/15209978/where-to-store-secret-keys-django either this or remove it
+# I did option one and it worked! 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
