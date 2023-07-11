f723a1b026de645cfb8fbf7dd08b9deccd7551cc
strikeouts27 <stribling.andrew@gmail.com>
2023-07-11 16:42:09 -0500

    part 7 complete customizing the admin page

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone 
+from django.contrib import admin 
+
 import datetime
  
 # Create your models here.
@@ -34,6 +36,16 @@ class Question(models.Model):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
