7af38e678dce8f849597158b85bf486d46edeff4
strikeouts27 <stribling.andrew@gmail.com>
2023-07-11 15:36:22 -0500

    part 5 complete, had to fix error with indentation

--- polls/views.py
+++ polls/views.py
@@ -12,20 +12,20 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
-def get_queryset(self):
-    """
-    Return the last five published questions (not including those set to be
-    published in the future).
-    """
-    return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
-        :5
-    ]
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 
-def get_queryset(self):
-    """
-    Excludes any questions that aren't published yet.
-    """
-    return Question.objects.filter(pub_date__lte=timezone.now())
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class DetailView(generic.DetailView):
